a529c504c5f74ab4076021aa2b0979eb137494b6
Richard Rzeszutek <richengguy@users.noreply.github.com>
2024-11-28 21:44:55 -0500

    Tweaking the project setup...

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,20 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMake
 
 
 class AbstractionsRecipe(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    settings = ["os", "compiler", "build_type", "arch"]
+    generators = ["CMakeToolchain", "CMakeDeps"]
 
-    def requirements(self) -> None:
-        ...
-
-    def build_requirements(self) -> None:
-        self.tool_requires("cmake/3.31.0")
+    tool_requires = ["cmake/3.31.0"]
+    requires = [
+        "blend2d/0.11.4",
+        "cli11/2.4.2",
+        "eigen/3.4.0",
+        "fmt/11.0.2",
+        "indicators/2.3",
+        "nlohmann_json/3.11.3",
+    ]
 
     def layout(self) -> None:
         cmake_layout(self)
@@ -18,3 +22,7 @@ class AbstractionsRecipe(ConanFile):
     def validate(self) -> None:
         # TODO: figure this out
         ...
+
+    def build(self) -> None:
+        cmake = CMake(self)
+        cmake.build()
