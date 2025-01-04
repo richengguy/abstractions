1a91dc862620d0cd2d06f8981239f488f5b0bd6f
Richard Rzeszutek <richengguy@users.noreply.github.com>
2025-01-03 21:15:04 -0500

    Making progress on the abstraction engine.

--- src/abstractions/engine.cpp
+++ src/abstractions/engine.cpp
@@ -2,6 +2,10 @@
 
 #include <abstractions/profile.h>
 #include <abstractions/threads/threadpool.h>
+#include <abstractions/render/renderer.h>
+
+#include <functional>
+#include <vector>
 
 namespace abstractions
 {
@@ -9,12 +13,39 @@ namespace abstractions
 namespace
 {
 
+/// @brief Contains the optimizer along with everything it needs to perform
+///     the "sample" and "optimize" operations.
+struct OptimizerPayload
+{
+    std::reference_wrapper<PgpeOptimizer> optimizer;
+    MatrixRef samples;
+    ColumnVectorRef costs;
+};
+
+/// @brief Contains everything needed to render a single image and compute the
+///     matching cost.
+struct RenderPayload
+{
+    std::reference_wrapper<const Image> reference;
+    std::vector<render::Renderer> renderers;
+    MatrixRef samples;
+    ColumnVectorRef costs;
+    const ImageComparison comparison_metric;
+};
+
 /// @brief Generate the set of samples needed for estimating sample costs.
 struct GenerateSolutionSamples : public threads::IJobFunction
 {
     Error operator()(threads::JobContext &ctx) const override
     {
-        return errors::no_error;
+        auto payload = ctx.Data<OptimizerPayload>();
+        if (!payload.has_value())
+        {
+            return payload.error();
+        }
+
+        auto &optim = payload->optimizer.get();
+        return optim.Sample(payload->samples);
     }
 };
 
@@ -23,7 +54,14 @@ struct RunOptimizer : public threads::IJobFunction
 {
     Error operator()(threads::JobContext &ctx) const override
     {
-        return errors::no_error;
+        auto payload = ctx.Data<OptimizerPayload>();
+        if (!payload.has_value())
+        {
+            return payload.error();
+        }
+
+        auto &optim = payload->optimizer.get();
+        return optim.Update(payload->samples, payload->costs);
     }
 };
 
@@ -33,6 +71,14 @@ struct RenderAndCompare : public threads::IJobFunction
 {
     Error operator()(threads::JobContext &ctx) const override
     {
+        auto payload = ctx.Data<RenderPayload>();
+        if (!payload.has_value())
+        {
+            return payload.error();
+        }
+
+        // TODO: Figure this out
+
         return errors::no_error;
     }
 };
@@ -120,6 +166,10 @@ Expected<OptimizationResult> Engine::GenerateAbstraction(const Image &reference)
     // Do the initial abstract shape generation to prime the optimizer with an
     // initial solution.
     render::PackedShapeCollection image_abstraction;
+
+    Matrix samples;
+    ColumnVector costs;
+
     OperationTiming init_timing;
     {
         Profile profiler{init_timing};
@@ -146,14 +196,66 @@ Expected<OptimizationResult> Engine::GenerateAbstraction(const Image &reference)
 
         image_abstraction = render::PackedShapeCollection(circles, rectangles, triangles);
         optimizer->Initialize(image_abstraction.AsPackedVector());
+
+        samples = Matrix::Zero(_config.num_samples, image_abstraction.TotalDimensions());
+        costs = ColumnVector::Zero(_config.num_samples);
     }
+
+    // Setup the thread payloads.
+    OptimizerPayload optim_payload{
+        .optimizer = *optimizer,
+        .samples = samples,
+        .costs = costs
+    };
+
+    RenderPayload render_payload{
+        .reference = reference,
+        .renderers = std::vector<render::Renderer>(),
+        .samples = samples,
+        .costs = costs,
+        .comparison_metric = _config.comparison_metric
+    };
+
+    // TODO: init the renderers
 
     // Now run the "sample->render->optimize" loop, keeping track of how the
     // solution is performing.
+    OperationTiming pgpe_timing;
+    OperationTiming render_timing;
+    OperationTiming sampling_timing;
 
     for (int i = 0; i < _config.max_iterations; i++)
     {
+        // Run the sampling step
+        auto sample_job = thread_pool.SubmitWithPayload<GenerateSolutionSamples>(0, optim_payload);
+        auto sample_result = sample_job.get();
 
+        if (sample_result.error) {
+            return errors::report<OptimizationResult>(sample_result.error);
+        }
+
+        sampling_timing.AddSample(sample_result.time);
+
+        // Render images from the generated samples and compute the costs.
+        std::vector<threads::Job::Future> futures;
+        for (int j = 0; j < _config.num_samples; j++)
+        {
+            auto render_job = thread_pool.SubmitWithPayload<RenderAndCompare>(j, render_payload);
+            // TODO: figure out how to wait on these in a reasonable way
+        }
+
+        // Run the optimizer and update its state.
+        auto update_job = thread_pool.SubmitWithPayload<GenerateSolutionSamples>(0, optim_payload);
+        auto update_result = update_job.get();
+
+        if (update_result.error) {
+            return errors::report<OptimizationResult>(update_result.error);
+        }
+
+        pgpe_timing.AddSample(update_result.time);
+
+        // Invoke any callbacks
+        // TODO
     }
 
     // Wrap things up
