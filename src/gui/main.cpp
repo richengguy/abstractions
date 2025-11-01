f953e270b137f9447e37d438aea226b3502a3d24
Richard Rzeszutek <richengguy@users.noreply.github.com>
2025-10-31 22:44:14 -0400

    Ran clang-format

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -8,16 +8,16 @@
 #include <iostream>
 
 int main() {
-  try {
-    webview::webview w(false, nullptr);
-    w.set_title("Basic Example");
-    w.set_size(480, 320, WEBVIEW_HINT_NONE);
-    w.set_html("Thanks for using webview!");
-    w.run();
-  } catch (const webview::exception &e) {
-    std::cerr << e.what() << '\n';
-    return 1;
-  }
+    try {
+        webview::webview w(false, nullptr);
+        w.set_title("Basic Example");
+        w.set_size(480, 320, WEBVIEW_HINT_NONE);
+        w.set_html("Thanks for using webview!");
+        w.run();
+    } catch (const webview::exception &e) {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
